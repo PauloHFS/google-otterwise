3e031bdbd7598fa274b032b528188964f27f06dd
Paulo Hernane <paulohernane10@gmail.com>
2022-01-06 20:53:06 -0300

    start webpage and api provider

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # google-otterwise
+
+2. Você vai criar a página do google. Através dessa api ([https://rapidapi.com/apigeek/api/google-search3/](https://rapidapi.com/apigeek/api/google-search3/)) é possível enviar qualquer informação que a api do google retorna os elementos da página.
+   1. Monte o layout bem parecido como o google faz.
+   2. Crie um campo de busca, onde o usuário pode digitar qualquer coisa e após clicar em um botão de pesquisar, deve ser exibido os resultados na tela.
---
+++ src/scripts/googleAPI.js
@@ -0,0 +1,21 @@
+import 'regenerator-runtime/runtime';
+require('dotenv').config();
+
+const getSearch = async query => {
+  try {
+    return await fetch(
+      `https://google-search3.p.rapidapi.com/api/v1/search/q=${query}`,
+      {
+        method: 'GET',
+        headers: {
+          'x-user-agent': 'desktop',
+          'x-proxy-location': 'BR',
+          'x-rapidapi-host': process.env.X_RAPIDAPI_HOST,
+          'x-rapidapi-key': process.env.X_RAPIDAPI_KEY,
+        },
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
